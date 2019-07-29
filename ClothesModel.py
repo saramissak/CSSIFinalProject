8d1fd04b941922d8536005ab70f303f6e0488e0e
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-29 16:21:23 -0700

    deleted

--- ClothesModel.py
+++ ClothesModel.py
@@ -5,6 +5,3 @@ class Clothes(ndb.Model):
     article_name = ndb.StringProperty(required = True)
     article_description = ndb.StringProperty(required = False)
     organization = ndb.StringProperty(required = False)
-
-    def getLines(self):
-        return self.line1 + " " + self.line2
