cd97947d30a2701aa2735338d78fe7774ca8a2d8
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-29 16:20:49 -0700

    udate requirement

--- ClothesModel.py
+++ ClothesModel.py
@@ -1,10 +1,10 @@
 from google.appengine.ext import ndb
 
 class Clothes(ndb.Model):
-    img_url = ndb.StringProperty()
-    article_name = ndb.StringProperty()
-    article_description = ndb.StringProperty()
-    organization = ndb.StringProperty()
+    img_url = ndb.StringProperty(required = True)
+    article_name = ndb.StringProperty(required = True)
+    article_description = ndb.StringProperty(required = False)
+    organization = ndb.StringProperty(required = False)
 
     def getLines(self):
         return self.line1 + " " + self.line2
